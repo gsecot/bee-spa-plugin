6620ef3e24fb312056b21209fc462b4c2b4da557
Giuseppe <peppe83@gmail.com>
2019-11-16 22:56:17 +0100

    setting up a node (express) server, in order to retrieve JWT

---
+++ server/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  ENDPOINT: 'https://auth.getbee.io/apiauth',
+  CLIENT_ID: 'cc5db5f6-bdf1-4d00-adb9-6fd94c45001d',
+  CLIENT_SECRET: 'iMmEWaYYEjUtrAQMs2BFK49pVXVw0AcOfKX7ThJvSEwOwjHFkhEq',
+}
